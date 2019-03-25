be026fa1f3e4ffde66614f09730ae6036f93fa98
unknown <jbsv43@sina.com>
2019-03-25 22:22:27 +0800

    add debug info

--- projects/TEMP/tests/conftest.py
+++ projects/TEMP/tests/conftest.py
@@ -17,12 +17,17 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     setattr(item, "rep_" + rep.when, rep)
     if rep.when == "call":
+        print("\nCase Duration: %ss ...%s" % (str(round(rep.duration, 2)), rep.outcome))
+        print(rep.keywords)
+        print(rep.location)
+        print(rep.nodeid)
+        print(rep.fspath)
         if rep.passed:
             pass_sum += 1
         elif rep.failed:
             fail_sum += 1
         elif rep.skipped:
             skip_sum += 1
-        print("\nCurrent Status: Pass - %d, Fail - %d, Skip - %d" % (pass_sum, fail_sum, skip_sum))
+        print("Current Status: Pass - %d, Fail - %d, Skip - %d\n" % (pass_sum, fail_sum, skip_sum))
         with open(stat_file, "w") as f:
             f.write("Pass,%d\nFail,%d\nSkip,%d\nEndTime,%s" % (pass_sum, fail_sum, skip_sum, datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")))
--- projects/TEMP/tests/test_sample.py
+++ projects/TEMP/tests/test_sample.py
@@ -1,6 +1,4 @@
-
-class Test_Sample():
-
+class TestSample:
 
     def test_answer1(self):
         assert 2+3 == 5
@@ -8,9 +6,8 @@ class Test_Sample():
     def test_answer2(self):
         assert 2 + 4 == 5
 
+    def test_answer3(self):
+        assert 2+3 == 5
 
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(3) == 4
+    def test_answer4(self):
+        assert 2 + 4 == 5
