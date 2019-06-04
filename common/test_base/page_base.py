c82128383f79a1834059d525ac15ca65912c5af8
bobjiangps <jbsv43@sina.com>
2019-06-04 14:31:00 +0800

    update page base

--- common/test_base/page_base.py
+++ common/test_base/page_base.py
@@ -1,6 +1,14 @@
 from common.behaviors.web_behaviors import WebBehaviors
 from configuration.config import LoadConfig
 from utils.yaml_helper import YamlHelper
+from common.elements.button import Button
+from common.elements.checkbox import Checkbox
+from common.elements.dropdown import Dropdown
+from common.elements.frame import Frame
+from common.elements.input import Input
+from common.elements.link import Link
+from common.elements.radiobutton import RadioButton
+from common.elements.static import Static
 import os
 
 
@@ -17,5 +25,26 @@ class PageBase:
         else:
             self.log.critical("no element definition file found!!: %s" % element_file)
 
-    def element(self, name):
+    def element_info(self, name):
         return self.elements[name]
+
+    def element(self, name, *args):
+        options = {
+            "button": Button(self.behavior, self.elements[name], *args),
+            "checkbox": Checkbox(self.behavior, self.elements[name], *args),
+            "dropdown": Dropdown(self.behavior, self.elements[name], *args),
+            "input": Input(self.behavior, self.elements[name], *args),
+            "link": Link(self.behavior, self.elements[name], *args),
+            "radiobutton": RadioButton(self.behavior, self.elements[name], *args),
+            "text": Static(self.behavior, self.elements[name], *args)
+        }
+        if self.elements[name]["type"].lower():
+            return options[self.elements[name]["type"].lower()]
+        else:
+            self.log.info("%s is not a valid element type..." % self.elements[name]["type"])
+
+    def frame(self, name, *args):
+        if self.elements[name]["type"].lower().find("frame") >= 0:
+            return Frame(self.behavior, self.elements[name], *args)
+        else:
+            self.log.info("%s is not a frame..." % self.elements[name]["value"])
