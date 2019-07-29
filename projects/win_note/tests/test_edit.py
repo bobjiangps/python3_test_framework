78941c939417bc5cdd6c0aac73a0ba6360acaf4c
bobjiangps <jbsv43@sina.com>
2019-07-29 16:43:20 +0800

    add win debug result

--- projects/win_note/tests/test_edit.py
+++ projects/win_note/tests/test_edit.py
@@ -9,7 +9,7 @@ class TestEdit(NoteWinTestCase):
             file_name = "save_as.txt"
             self.edit_page.remove_file_if_exists_in_test_data(file_name)
 
-        with self.steps()
+        with self.steps():
             texts = "this is a test"
             self.edit_page.input_texts_into_edit_then_save(texts, file_name)
 
