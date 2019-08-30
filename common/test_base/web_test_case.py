6c20694da69bdd336dbdd6ee7843f18e0ab690a8
bobjiangps <jbsv43@sina.com>
2019-08-30 14:09:51 +0800

    support video for web and win

--- common/test_base/web_test_case.py
+++ common/test_base/web_test_case.py
@@ -2,6 +2,10 @@ from utils.selenium_helper import SeleniumHelper
 from configuration.config import LoadConfig
 from common.test_base.logged_test_case import LoggedTestCase
 from selenium.common.exceptions import WebDriverException
+from utils.video_helper import VideoHelper
+import os
+import json
+import datetime
 
 
 class WebTestCase(LoggedTestCase):
@@ -19,7 +23,31 @@ class WebTestCase(LoggedTestCase):
 
     def setup_method(self):
         super().setup_method()
+        self.video_config = LoadConfig.load_config("video")
+        if self.video_config["record"]:
+            video_folder_path = os.path.join(os.getcwd(), "projects", LoadConfig.load_config("project"), "test_reports", "videos")
+            if not os.path.exists(video_folder_path):
+                os.mkdir(video_folder_path)
+            self.video_file_path = os.path.join(video_folder_path, "%s.mp4" % datetime.datetime.now().strftime("%Y%m%d%H%M%S%f"))
+            self.vh = VideoHelper(out_file=self.video_file_path)
+            self.vh.start_recording_screen()
         self.browser_home_page()
+
+    def teardown_method(self):
+        super().teardown_method()
+        if self.video_config["record"]:
+            self.vh.stop_recording_screen()
+            self.log.info("video captured")
+            if self.video_config["only_record_failed"]:
+                stat_file = os.path.join(os.getcwd(), "projects", LoadConfig.load_config("project"), "test_reports", "stat.json")
+                with open(stat_file, "r") as f:
+                    current_stat = json.load(f)
+                current_test = os.environ.get('PYTEST_CURRENT_TEST').split('::')
+                current_test_file = current_test[0].split(os.sep)[-1].split(".")[0]
+                current_test_name = current_test[-1].split(" ")[0]
+                if current_stat["Details"][current_test_file][current_test_name].lower() == "pass":
+                    os.remove(self.video_file_path)
+                    self.log.info("remove video because current test pass")
 
     @classmethod
     def start_browser(cls):
--- common/test_base/win_test_case.py
+++ common/test_base/win_test_case.py
@@ -2,7 +2,10 @@ from utils.winauto_helper import WinautoHelper
 from utils.yaml_helper import YamlHelper
 from configuration.config import LoadConfig
 from common.test_base.logged_test_case import LoggedTestCase
+from utils.video_helper import VideoHelper
 import os
+import json
+import datetime
 
 
 class WinTestCase(LoggedTestCase):
@@ -17,6 +20,34 @@ class WinTestCase(LoggedTestCase):
     def teardown_class(cls):
         super().teardown_class()
         WinautoHelper.close_driver()
+
+    def setup_method(self):
+        super().setup_method()
+        self.video_config = LoadConfig.load_config("video")
+        if self.video_config["record"]:
+            video_folder_path = os.path.join(os.getcwd(), "projects", LoadConfig.load_config("project"), "test_reports", "videos")
+            if not os.path.exists(video_folder_path):
+                os.mkdir(video_folder_path)
+            self.video_file_path = os.path.join(video_folder_path, "%s.mp4" % datetime.datetime.now().strftime("%Y%m%d%H%M%S%f"))
+            self.vh = VideoHelper(out_file=self.video_file_path)
+            self.vh.start_recording_screen()
+        self.browser_home_page()
+
+    def teardown_method(self):
+        super().teardown_method()
+        if self.video_config["record"]:
+            self.vh.stop_recording_screen()
+            self.log.info("video captured")
+            if self.video_config["only_record_failed"]:
+                stat_file = os.path.join(os.getcwd(), "projects", LoadConfig.load_config("project"), "test_reports", "stat.json")
+                with open(stat_file, "r") as f:
+                    current_stat = json.load(f)
+                current_test = os.environ.get('PYTEST_CURRENT_TEST').split('::')
+                current_test_file = current_test[0].split(os.sep)[-1].split(".")[0]
+                current_test_name = current_test[-1].split(" ")[0]
+                if current_stat["Details"][current_test_file][current_test_name].lower() == "pass":
+                    os.remove(self.video_file_path)
+                    self.log.info("remove video because current test pass")
 
     @classmethod
     def start_app(cls):
