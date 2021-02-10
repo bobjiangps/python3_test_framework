3c0af37b4a32928a9d9c4b8b56e02d0818972ebb
bobjiangps <jbsv43@sina.com>
2021-02-10 15:25:28 +0800

    update yaml helper for yaml file include another yaml file

--- utils/yaml_helper.py
+++ utils/yaml_helper.py
@@ -1,17 +1,34 @@
 import yaml
+# import os
+
+# the following class is used for including another yaml file, like: "c: !include t.yaml"
+# class Loader(yaml.Loader):
+#
+#     def __init__(self, stream):
+#         self._root = os.path.split(stream.name)[0]
+#
+#         super().__init__(stream)
+#
+#     def include(self, node):
+#         filename = os.path.join(self._root, self.construct_scalar(node))
+#
+#         with open(filename, 'r') as f:
+#             return yaml.load(f, Loader)
+#
+#
+# Loader.add_constructor('!include', Loader.include)
 
 
 class YamlHelper:
 
     @classmethod
     def load_yaml(cls, file_path):
-        rf = open(file_path, "rb")
-        ret = yaml.load(rf, Loader=yaml.SafeLoader)
-        rf.close()
-        return ret
+        with open(file_path, "r") as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+            # data = yaml.load(f, Loader=Loader) # the class above
+        return data
 
     @classmethod
     def dump_yaml(cls, file_path, data):
-        wf = open(file_path, "w")
-        yaml.dump(data, wf)
-        wf.close()
+        with open(file_path, "w") as f:
+            yaml.dump(data, f)
